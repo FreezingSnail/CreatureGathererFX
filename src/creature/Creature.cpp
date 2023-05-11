6066371b52c4867b4149d5d5f1c5dfc7aa2308b9
FreezingSnail <smogonv3@gmail.com>
2023-05-11 17:02:19 -0400

    rewrite moves from int to class and add packed data

--- src/creature/Creature.cpp
+++ src/creature/Creature.cpp
@@ -1,96 +1,95 @@
 #include "Creature.hpp"
-#include "../opponent/OpponentSeed.hpp"
 #include "../data/Creatures.hpp"
 #include "../lib/Move.hpp"
-#include "../sprites/sprites.hpp"
+#include "../opponent/OpponentSeed.hpp"
 #include "../sprites/creatureSprites.h"
-#include <avr/pgmspace.h> 
+#include "../sprites/sprites.hpp"
+#include <avr/pgmspace.h>
 
-//This will need to load the creature seed from the progmemstore
+// This will need to load the creature seed from the progmemstore
 const CreatureData_t getCreatureFromStore(uint8_t id) {
-	CreatureData_t cseed;
-    memcpy_P(&cseed, &CreatureData[0], sizeof(CreatureData_t));
-	return cseed;
+  CreatureData_t cseed;
+  memcpy_P(&cseed, &CreatureData[0], sizeof(CreatureData_t));
+  return cseed;
 }
 
 Creature::Creature() {
-	this->statlist.attack = 0;
-	this->statlist.defense = 0;
-	this->statlist.speed = 0;
-	this->statlist.hp = 0;
-	this->statlist.spcAtk = 0;
-	this->statlist.spcDef = 0;
+  this->statlist.attack = 0;
+  this->statlist.defense = 0;
+  this->statlist.speed = 0;
+  this->statlist.hp = 0;
+  this->statlist.spcAtk = 0;
+  this->statlist.spcDef = 0;
 }
 
-//todo(snail)
-// maybe I should move this out into an abstraction so its easier to change
+// todo(snail)
+//  maybe I should move this out into an abstraction so its easier to change
 void Creature::load(CreatureData_t seed) {
-	this->id = static_cast<uint8_t>((seed.id));
-	this->setStats(seed);
-	//Need some kind of default setting for moves ?
-	this->loadMoves(seed);
-	this->loadSprite(seed);
+  this->id = static_cast<uint8_t>((seed.id));
+  this->setStats(seed);
+  // Need some kind of default setting for moves ?
+  this->loadMoves(seed);
+  this->loadSprite(seed);
 }
 
-//00,id1,lvl1,move11,move12,move13,move14,
-void Creature::loadFromOpponentSeed(uint32_t seed){
-	CreatureData_t cSeed = getCreatureFromStore(parseOpponentCreatureSeedID(seed));
-	this->id = static_cast<uint8_t>((cSeed.id));
-	this->loadTypes(cSeed);
-	this->level = parseOpponentCreatureSeedlvl(seed);
-	this->setStats(cSeed);
-	this->setMove(parseOpponentCreatureSeedMove(seed, 0), 0);
-	this->setMove(parseOpponentCreatureSeedMove(seed, 1), 1);
-	this->setMove(parseOpponentCreatureSeedMove(seed, 2), 2);
-	this->setMove(parseOpponentCreatureSeedMove(seed, 3), 3);
-	this->loadSprite(cSeed);
+// 00,id1,lvl1,move11,move12,move13,move14,
+void Creature::loadFromOpponentSeed(uint32_t seed) {
+  CreatureData_t cSeed =
+      getCreatureFromStore(parseOpponentCreatureSeedID(seed));
+  this->id = static_cast<uint8_t>((cSeed.id));
+  this->loadTypes(cSeed);
+  this->level = parseOpponentCreatureSeedlvl(seed);
+  this->setStats(cSeed);
+  this->setMove(parseOpponentCreatureSeedMove(seed, 0), 0);
+  this->setMove(parseOpponentCreatureSeedMove(seed, 1), 1);
+  this->setMove(parseOpponentCreatureSeedMove(seed, 2), 2);
+  this->setMove(parseOpponentCreatureSeedMove(seed, 3), 3);
+  this->loadSprite(cSeed);
 }
 
 void Creature::loadMoves(CreatureData_t seed) {
-	this->setMove(static_cast<uint8_t>((seed.move1)), 0);
-	this->setMove(static_cast<uint8_t>((seed.move2)), 1);
-	this->setMove(static_cast<uint8_t>((seed.move3)), 2);
-	this->setMove(static_cast<uint8_t>((seed.move4)), 3);
+  this->setMove(static_cast<uint8_t>((seed.move1)), 0);
+  this->setMove(static_cast<uint8_t>((seed.move2)), 1);
+  this->setMove(static_cast<uint8_t>((seed.move3)), 2);
+  this->setMove(static_cast<uint8_t>((seed.move4)), 3);
 }
 
 void Creature::loadSprite(CreatureData_t seed) {
-	this->sprite = creatureSprites[static_cast<uint8_t>((seed.id))];
+  this->sprite = creatureSprites[static_cast<uint8_t>((seed.id))];
 }
 
 void Creature::loadTypes(CreatureData_t seed) {
-	this->types = DualType((Type)static_cast<uint8_t>((seed.type1)), (Type)static_cast<uint8_t>((seed.type2)));
+  this->types = DualType((Type) static_cast<uint8_t>((seed.type1)),
+                         (Type) static_cast<uint8_t>((seed.type2)));
 }
 
 // should prob have error checking but w/e
-void Creature::setMove(uint8_t move, uint8_t slot){
-	this->moves[slot] = move;
+void Creature::setMove(uint8_t move, uint8_t slot) {
+  this->moves[slot] = move;
+  this->moveList[slot] = Move(getMovePack(move));
 }
 
 void Creature::setStats(CreatureData_t seed) {
-	this->statlist.attack 	= seedToStat((seed.atkSeed));
-	this->statlist.defense 	= seedToStat((seed.defSeed));
-	this->statlist.speed 	= seedToStat((seed.spdSeed));
-	this->statlist.hp 		= seedToStat((seed.hpSeed));
-	this->statlist.spcAtk 	= seedToStat((seed.spcAtkSeed));
-	this->statlist.spcDef 	= seedToStat((seed.spcDefSeed));
-
+  this->statlist.attack = seedToStat((seed.atkSeed));
+  this->statlist.defense = seedToStat((seed.defSeed));
+  this->statlist.speed = seedToStat((seed.spdSeed));
+  this->statlist.hp = seedToStat((seed.hpSeed));
+  this->statlist.spcAtk = seedToStat((seed.spcAtkSeed));
+  this->statlist.spcDef = seedToStat((seed.spcDefSeed));
 }
 
 // some ai to find best advantage should move this out of this class though
 // going to need this for the opponent ai
-uint8_t Creature::getAdvantage(DualType opponent) {
-	return 0;
-}
+uint8_t Creature::getAdvantage(DualType opponent) { return 0; }
 
-uint8_t Creature::getMove(uint8_t slot) {
-	return this->moves[slot];
-}
+uint8_t Creature::getMove(uint8_t slot) { return this->moves[slot]; }
 
-bool Creature::moveTypeBonus(uint8_t move) {
-	return this->types.getType1() == (Type)getMoveType(move) || this->types.getType2() == (Type)getMoveType(move);
+bool Creature::moveTypeBonus(uint8_t index) {
+  return this->types.getType1() == (Type)this->moveList[index].getMoveType() ||
+         this->types.getType2() == (Type)this->moveList[index].getMoveType();
 }
 
 uint8_t Creature::seedToStat(uint8_t seed) {
-	return seed;
-	//return (2*this->level)*(seed/3);
+  return seed;
+  // return (2*this->level)*(seed/3);
 }
--- src/lib/Move.hpp
+++ src/lib/Move.hpp
@@ -1,74 +1,137 @@
 #pragma once
+#include "Type.hpp"
+#include <avr/pgmspace.h>
 #include <stdint.h>
-#include <avr/pgmspace.h> 
-#include "Moves.hpp"
 
 #ifdef CLI
-	#include <iostream>
-	#include <bitset>
+#include <bitset>
+#include <iostream>
 #endif
 
-static const uint8_t* moveList[32] =	{ &m00, &m01, &m02, &m03, &m04, &m05, &m06, &m07, &m08, &m09, 
-										&m10, &m11, &m12, &m13, &m14, &m15, &m16, &m17, &m18, &m19, 
-										&m20, &m21, &m22, &m23, &m24, &m25, &m26, &m27, &m28, &m29, 
-										&m31, &m31 };
+// move
+//  00    000    000
+//  id#   type   power
 
-const char move0[] PROGMEM = "move1";
-const char move1[] PROGMEM = "move2";
-const char move2[] PROGMEM = "move3";
-const char move3[] PROGMEM = "move4";
-const char move4[] PROGMEM = "move5";
-const char move5[] PROGMEM = "move6";
-const char move6[] PROGMEM = "move7";
-const char move7[] PROGMEM = "move8";
-const char move8[] PROGMEM = "move9";
-const char move9[] PROGMEM = "move10";
-const char move10[] PROGMEM = "move11";
-const char move11[] PROGMEM = "move12";
-const char move12[] PROGMEM = "move13";
-const char move13[] PROGMEM = "move14";
-const char move14[] PROGMEM = "move15";
-const char move15[] PROGMEM = "move16";
-const char move16[] PROGMEM = "move17";
-const char move17[] PROGMEM = "move18";
-const char move18[] PROGMEM = "move19";
-const char move19[] PROGMEM = "move20";
-const char move20[] PROGMEM = "move21";
-const char move21[] PROGMEM = "move22";
-const char move22[] PROGMEM = "move23";
-const char move23[] PROGMEM = "move24";
-const char move24[] PROGMEM = "move25";
-const char move25[] PROGMEM = "move26";
-const char move26[] PROGMEM = "move27";
-const char move27[] PROGMEM = "move28";
-const char move28[] PROGMEM = "move29";
-const char move29[] PROGMEM = "move30";
-const char move30[] PROGMEM = "move31";
-const char move31[] PROGMEM = "move32";
+struct MoveBitSet {
+  uint8_t type : 3;
+  uint8_t power : 4; // maybe this should be 2 bit 0-3 for 4 power levels? ->
+                     // could open up room for effects
+  uint8_t physical : 1;
+};
 
-const char* const moveNames[] PROGMEM = {move1, move2, move3, move4,  move5, 
-					move6, move7, move8, move9, move10, move11, move12, move13, move14, 
-					move15, move16, move17, move18, move19, move20, move21, move22, move23, 
-					move24, move25, move26, move27, move28, move29, move30, move31};
-//move 
-// 00    000    000    
-// id#   type   power 
-static uint8_t getMovePower(uint8_t move){
-	return move & 0b00000111;
+const MoveBitSet movePack[32] PROGMEM = {
+    {0, 10, 0}, {0, 5, 1},  {0, 7, 1},  {0, 10, 1}, {1, 4, 0},  {1, 7, 0},
+    {1, 10, 0}, {1, 7, 1},  {2, 5, 0},  {2, 10, 0}, {2, 10, 1}, {2, 7, 1},
+    {3, 7, 0},  {3, 5, 1},  {3, 7, 1},  {3, 10, 1}, {4, 8, 0},  {4, 12, 0},
+    {4, 12, 1}, {4, 6, 1},  {5, 7, 0},  {5, 12, 0}, {5, 12, 1}, {5, 7, 1},
+    {6, 6, 0},  {6, 12, 0}, {6, 12, 1}, {6, 8, 1},  {7, 8, 0},  {7, 12, 0},
+    {7, 10, 1}, {7, 14, 1},
+};
+
+const char move0[] PROGMEM = "smite";
+const char move1[] PROGMEM = "thought";
+const char move2[] PROGMEM = "wisper";
+const char move3[] PROGMEM = "blast";
+const char move4[] PROGMEM = "splash";
+const char move5[] PROGMEM = "squirt";
+const char move6[] PROGMEM = "soak";
+const char move7[] PROGMEM = "current";
+const char move8[] PROGMEM = "breeze";
+const char move9[] PROGMEM = "bellow";
+const char move10[] PROGMEM = "torrent";
+const char move11[] PROGMEM = "blow";
+const char move12[] PROGMEM = "dirtbeam";
+const char move13[] PROGMEM = "slipfall";
+const char move14[] PROGMEM = "dirtfall";
+const char move15[] PROGMEM = "shatter";
+const char move16[] PROGMEM = "burn";
+const char move17[] PROGMEM = "melt";
+const char move18[] PROGMEM = "torch";
+const char move19[] PROGMEM = "stoke";
+const char move20[] PROGMEM = "bolt";
+const char move21[] PROGMEM = "plasma";
+const char move22[] PROGMEM = "coil";
+const char move23[] PROGMEM = "zap";
+const char move24[] PROGMEM = "pollen";
+const char move25[] PROGMEM = "pollenbeam";
+const char move26[] PROGMEM = "seedfall";
+const char move27[] PROGMEM = "root";
+const char move28[] PROGMEM = "beam";
+const char move29[] PROGMEM = "elderBeam";
+const char move30[] PROGMEM = "slam";
+const char move31[] PROGMEM = "elderSlam";
+
+const char *const moveNames[32] PROGMEM = {
+    move1,  move2,  move3,  move4,  move5,  move6,  move7,  move8,
+    move9,  move10, move11, move12, move13, move14, move15, move16,
+    move17, move18, move19, move20, move21, move22, move23, move24,
+    move25, move26, move27, move28, move29, move30, move31};
+
+static MoveBitSet getMovePack(uint8_t index) {
+  MoveBitSet move;
+  memcpy_P(&move, &movePack[index], sizeof(MoveBitSet));
+  return move;
 }
 
-static uint8_t getMoveType(uint8_t move){
+class Move {
+private:
+  uint8_t move;
 
-	return ((move & 0b00111000) >> 3);
-}
+  // 000 0000 0
+  static const uint8_t TypeMask = 0b11100000;
+  static const uint8_t PowerMask = 0b00011110;
+  static const uint8_t PhysMask = 0b00000001;
+  static const uint8_t TypeShift = 5;
+  static const uint8_t PowerShift = 1;
 
-// Each type has 4 moves 
-// The posistion of these 4 is stored in the first 2 bits
-// The index of the move globally is a function of its type and id
-static uint8_t getMoveID(uint8_t move){
-	return (((4) * ((move >> 3) & 0b111)) + (move >> 6));
-}
+  constexpr uint8_t packMove(MoveBitSet move) {
+    return (move.type << TypeShift) | (move.power << PowerShift) |
+           move.physical;
+  }
 
-static uint8_t getMove(uint8_t index) {
-	return pgm_read_word_near(moveList[index]);
-}
+public:
+  constexpr Move() : move(0) {}
+
+  constexpr Move(MoveBitSet movePack) : move(packMove(movePack)) {}
+
+  constexpr uint8_t getMovePower() { return (this->move & PowerMask) >> 1; }
+
+  constexpr uint8_t getMoveType() { return ((this->move & TypeMask) >> 5); }
+
+  constexpr bool isPhysical() { return (this->move & PhysMask); }
+};
+
+// gana need 32 of these move lists, for each creature
+const PROGMEM uint32_t movelistDebug = 0b1111111111111111111111111111111;
+const PROGMEM uint32_t movelist0 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist1 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist2 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist3 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist4 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist5 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist6 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist7 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist8 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist9 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist10 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist11 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist12 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist13 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist14 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist15 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist16 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist17 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist18 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist19 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist20 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist21 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist22 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist23 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist24 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist25 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist26 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist27 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist28 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist29 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist30 = 0b00000000000000000000000000000000;
+const PROGMEM uint32_t movelist31 = 0b00000000000000000000000000000000;
