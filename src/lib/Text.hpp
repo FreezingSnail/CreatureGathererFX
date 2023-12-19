2e36ca65573cbaaa491ba8d74b31f3352220fc2b
FreezingSnail <smogonv3@gmail.com>
2023-12-19 15:19:56 -0500

    string print optimization

--- src/lib/Text.hpp
+++ src/lib/Text.hpp
@@ -6,10 +6,10 @@
 static void printString(Font4x6 &font, uint24_t addr, uint8_t x, uint8_t y) {
     uint8_t index = 0;
     unsigned char character;
-    do {
-        character = FX::readIndexedUInt8(addr, index);
+    FX::seekData(addr);
+    while ((character = FX::readPendingUInt8()) != '\0') {
         font.printChar(character, x, y);
-        index++;
         x += 5;
-    } while (character != '\0');
+    }
+    (void)FX::readEnd();
 }
