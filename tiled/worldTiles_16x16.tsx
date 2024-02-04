1c13ef5681c7311b2b665b05093a0768796b6de0
FreezingSnail <smogonv3@gmail.com>
2024-02-04 17:22:51 -0500

    tield stuff

--- tiled/worldTiles_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="worldTiles_16x16" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../src/fxdata/images/worldTiles_16x16.png" width="64" height="64"/>
-</tileset>
