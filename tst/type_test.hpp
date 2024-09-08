4f5d90d3d354ea58996c8ff15b6bb1da418fb383
FreezingSnail <smogonv3@gmail.com>
2024-09-08 10:19:00 -0400

    more tests

--- tst/type_test.hpp
+++ tst/type_test.hpp
@@ -20,6 +20,30 @@ void TypeTest(TestSuite &t) {
     test.assert(dt2.hasType(Type::FIRE), true, "Has Fire Type");
     test.assert(dt2.hasType(Type::EARTH), true, "Has Earth Type");
 
+    dt = DualType(Type::EARTH, Type::FIRE);
+    test.assert(dt.getType1(), Type::EARTH, "Primary Type");
+    test.assert(dt.getType2(), Type::FIRE, "Secondary Type");
+
+    dt = DualType(Type::SPIRIT, Type::WATER);
+    test.assert(dt.getType1(), Type::SPIRIT, "Primary Type");
+    test.assert(dt.getType2(), Type::WATER, "Secondary Type");
+
+    dt = DualType(Type::WIND, Type::EARTH);
+    test.assert(dt.getType1(), Type::WIND, "Primary Type");
+    test.assert(dt.getType2(), Type::EARTH, "Secondary Type");
+
+    dt = DualType(Type::LIGHTNING, Type::PLANT);
+    test.assert(dt.getType1(), Type::LIGHTNING, "Primary Type");
+    test.assert(dt.getType2(), Type::PLANT, "Secondary Type");
+
+    dt = DualType(Type::ELDER, Type::NONE);
+    test.assert(dt.getType1(), Type::ELDER, "Primary Type");
+    test.assert(dt.getType2(), Type::NONE, "Secondary Type");
+
+    dt = DualType(Type::NONE, Type::NONE);
+    test.assert(dt.getType1(), Type::NONE, "Primary Type");
+    test.assert(dt.getType2(), Type::NONE, "Secondary Type");
+
     t.addTest(test);
 }
 
